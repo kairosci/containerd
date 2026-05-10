kairosci/containerd

commit c15ec248571cecd7b073dfaacaf932cfb09ba409

Add server plugins for grpc and ttrpc
Migrate configuration and move server initialization

Signed-off-by: Derek McGowan <derek@mcg.dev>

plugins/server/grpc/plugin.go
@@ -1,0 +1,292 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package grpc
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/containerd/errdefs"
+	"github.com/containerd/log"
+	"github.com/containerd/plugin"
+	"github.com/containerd/plugin/registry"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/stats"
+
+	"github.com/containerd/containerd/v2/defaults"
+	"github.com/containerd/containerd/v2/internal/wintls"
+	"github.com/containerd/containerd/v2/pkg/sys"
+	"github.com/containerd/containerd/v2/plugins"
+	"github.com/containerd/containerd/v2/plugins/server/internal"
+)
+
+type config struct {
+	Address        string `toml:"address"`
+	UID            int    `toml:"uid"`
+	GID            int    `toml:"gid"`
+	MaxRecvMsgSize int    `toml:"max_recv_message_size"`
+	MaxSendMsgSize int    `toml:"max_send_message_size"`
+}
+
+type tcpConfig struct {
+	Address        string `toml:"address"`
+	TLSCA          string `toml:"tls_ca"`
+	TLSCert        string `toml:"tls_cert"`
+	TLSKey         string `toml:"tls_key"`
+	TLSCName       string `toml:"tls_common_name"`
+	MaxRecvMsgSize int    `toml:"max_recv_message_size"`
+	MaxSendMsgSize int    `toml:"max_send_message_size"`
+}
+
+func init() {
+	registry.Register(&plugin.Registration{
+		Type: plugins.ServerPlugin,
+		ID:   "grpc",
+		Requires: []plugin.Type{
+			plugins.GRPCPlugin,
+			plugins.MetricsPlugin,
+		},
+		Config: &config{
+			Address:        defaults.DefaultAddress,
+			MaxRecvMsgSize: defaults.DefaultMaxRecvMsgSize,
+			MaxSendMsgSize: defaults.DefaultMaxSendMsgSize,
+		},
+		InitFn: func(ic *plugin.InitContext) (any, error) {
+			c := ic.Config.(*config)
+			if c.Address == "" {
+				return nil, fmt.Errorf("grpc address cannot be empty: %w", errdefs.ErrInvalidArgument)
+			}
+
+			var (
+				streamOpts              = []grpc.StreamServerInterceptor{streamNamespaceInterceptor}
+				unaryOpts               = []grpc.UnaryServerInterceptor{unaryNamespaceInterceptor}
+				serverOpts              = []grpc.ServerOption{}
+				prometheusServerMetrics *grpc_prometheus.ServerMetrics // This should be grpc handler
+			)
+
+			if p, err := ic.GetByID(plugins.MetricsPlugin, "grpc-prometheus"); err == nil {
+				prometheusServerMetrics = p.(*grpc_prometheus.ServerMetrics)
+				streamOpts = append(streamOpts, prometheusServerMetrics.StreamServerInterceptor())
+				unaryOpts = append(unaryOpts, prometheusServerMetrics.UnaryServerInterceptor())
+			}
+
+			if p, err := ic.GetByID(plugins.MetricsPlugin, "grpc-otel"); err == nil {
+				serverOpts = append(serverOpts, grpc.StatsHandler(p.(stats.Handler)))
+			}
+
+			serverOpts = append(serverOpts, grpc.ChainStreamInterceptor(streamOpts...))
+			serverOpts = append(serverOpts, grpc.ChainUnaryInterceptor(unaryOpts...))
+			if c.MaxRecvMsgSize > 0 {
+				serverOpts = append(serverOpts, grpc.MaxRecvMsgSize(c.MaxRecvMsgSize))
+			}
+			if c.MaxSendMsgSize > 0 {
+				serverOpts = append(serverOpts, grpc.MaxSendMsgSize(c.MaxSendMsgSize))
+			}
+
+			// grpcService allows GRPC services to be registered with the underlying server
+			type grpcService interface {
+				Register(*grpc.Server) error
+			}
+
+			s := grpc.NewServer(serverOpts...)
+			ps, err := ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
+			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
+				return nil, err
+			}
+			for _, p := range ps {
+				if gs, ok := p.(grpcService); ok {
+					if err := gs.Register(s); err != nil {
+						return nil, fmt.Errorf("failed to register grpc service: %w", err)
+					}
+				}
+			}
+			if prometheusServerMetrics != nil {
+				prometheusServerMetrics.InitializeMetrics(s)
+			}
+			return grpcServer{
+				Server: s,
+				config: *c,
+			}, nil
+		},
+	})
+
+	registry.Register(&plugin.Registration{
+		Type: plugins.ServerPlugin,
+		ID:   "grpc-tcp",
+		Requires: []plugin.Type{
+			plugins.GRPCPlugin,
+			plugins.MetricsPlugin,
+		},
+		Config: &tcpConfig{
+			MaxRecvMsgSize: defaults.DefaultMaxRecvMsgSize,
+			MaxSendMsgSize: defaults.DefaultMaxSendMsgSize,
+		},
+		InitFn: func(ic *plugin.InitContext) (any, error) {
+			c := ic.Config.(*tcpConfig)
+			if c.Address == "" {
+				return nil, plugin.ErrSkipPlugin
+			}
+
+			var (
+				streamOpts              = []grpc.StreamServerInterceptor{streamNamespaceInterceptor}
+				unaryOpts               = []grpc.UnaryServerInterceptor{unaryNamespaceInterceptor}
+				serverOpts              = []grpc.ServerOption{}
+				prometheusServerMetrics *grpc_prometheus.ServerMetrics // This should be grpc handler
+			)
+
+			if p, err := ic.GetByID(plugins.MetricsPlugin, "grpc-prometheus"); err == nil {
+				prometheusServerMetrics = p.(*grpc_prometheus.ServerMetrics)
+				streamOpts = append(streamOpts, prometheusServerMetrics.StreamServerInterceptor())
+				unaryOpts = append(unaryOpts, prometheusServerMetrics.UnaryServerInterceptor())
+				prometheusServerMetrics.InitializeMetrics(nil)
+			}
+
+			if p, err := ic.GetByID(plugins.MetricsPlugin, "grpc-otel"); err == nil {
+				serverOpts = append(serverOpts, grpc.StatsHandler(p.(stats.Handler)))
+			}
+
+			serverOpts = append(serverOpts, grpc.ChainStreamInterceptor(streamOpts...))
+			serverOpts = append(serverOpts, grpc.ChainUnaryInterceptor(unaryOpts...))
+
+			if c.MaxRecvMsgSize > 0 {
+				serverOpts = append(serverOpts, grpc.MaxRecvMsgSize(c.MaxRecvMsgSize))
+			}
+			if c.MaxSendMsgSize > 0 {
+				serverOpts = append(serverOpts, grpc.MaxSendMsgSize(c.MaxSendMsgSize))
+			}
+
+			if c.TLSCert != "" {
+				log.G(ic.Context).Info("setting up tls on tcp GRPC services...")
+
+				tlsCert, err := tls.LoadX509KeyPair(c.TLSCert, c.TLSKey)
+				if err != nil {
+					return nil, err
+				}
+				tlsConfig := &tls.Config{Certificates: []tls.Certificate{tlsCert}}
+
+				if c.TLSCA != "" {
+					caCertPool := x509.NewCertPool()
+					caCert, err := os.ReadFile(c.TLSCA)
+					if err != nil {
+						return nil, fmt.Errorf("failed to load CA file: %w", err)
+					}
+					caCertPool.AppendCertsFromPEM(caCert)
+					tlsConfig.ClientCAs = caCertPool
+					tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+				}
+				serverOpts = append(serverOpts, grpc.Creds(credentials.NewTLS(tlsConfig)))
+			} else if c.TLSCName != "" {
+				tlsConfig, CA, res, err :=
+					wintls.SetupTLSFromWindowsCertStore(ic.Context, c.TLSCName)
+				if err != nil {
+					return nil, fmt.Errorf("failed to setup TLS from Windows cert store: %w", err)
+				}
+				// Cache resource for cleanup (Windows only)
+				setTLSResource(res)
+				if CA != nil {
+					tlsConfig.ClientCAs = CA
+					tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+				}
+				serverOpts = append(serverOpts, grpc.Creds(credentials.NewTLS(tlsConfig)))
+			}
+
+			// tcpService allows GRPC services to be registered with the underlying tcp server
+			type tcpService interface {
+				RegisterTCP(*grpc.Server) error
+			}
+
+			s := grpc.NewServer(serverOpts...)
+			ps, err := ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
+			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
+				return nil, err
+			}
+			var hasService bool
+			for _, p := range ps {
+				if gs, ok := p.(tcpService); ok {
+					if err := gs.RegisterTCP(s); err != nil {
+						return nil, fmt.Errorf("failed to register grpc service: %w", err)
+					}
+					hasService = true
+				}
+			}
+			if !hasService {
+				return nil, fmt.Errorf("no tcp grpc services configured: %w", plugin.ErrSkipPlugin)
+			}
+			if prometheusServerMetrics != nil {
+				prometheusServerMetrics.InitializeMetrics(s)
+			}
+			return tcpServer{
+				Server: s,
+				config: *c,
+			}, nil
+		},
+	})
+}
+
+type grpcServer struct {
+	*grpc.Server
+	config config
+}
+
+func (s grpcServer) Start(ctx context.Context) error {
+	log.G(ctx).WithField("address", s.config.Address).WithField("uid", s.config.UID).WithField("gid", s.config.GID).Info("starting GRPC server")
+	l, err := sys.GetLocalListener(s.config.Address, s.config.UID, s.config.GID)
+	if err != nil {
+		return fmt.Errorf("failed to get listener for main endpoint: %w", err)
+	}
+
+	internal.Serve(ctx, l, s.Serve)
+
+	return nil
+}
+func (s grpcServer) Close() error {
+	s.Stop()
+
+	return nil
+}
+
+type tcpServer struct {
+	*grpc.Server
+	config tcpConfig
+}
+
+func (s tcpServer) Start(ctx context.Context) error {
+	l, err := net.Listen("tcp", s.config.Address)
+	if err != nil {
+		return fmt.Errorf("failed to get listener for TCP grpc endpoint: %w", err)
+	}
+
+	internal.Serve(ctx, l, s.Serve)
+
+	return nil
+}
+
+func (s tcpServer) Close() error {
+	s.Stop()
+
+	// Clean up TLS resources (Windows only)
+	cleanupTLSResources()
+
+	return nil
+}

plugins/server/ttrpc/plugin.go
@@ -1,0 +1,128 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package ttrpc
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+
+	"github.com/containerd/containerd/v2/defaults"
+	"github.com/containerd/containerd/v2/pkg/sys"
+	"github.com/containerd/containerd/v2/plugins"
+	"github.com/containerd/containerd/v2/plugins/server/internal"
+	"github.com/containerd/plugin"
+	"github.com/containerd/plugin/registry"
+	"github.com/containerd/ttrpc"
+)
+
+type config struct {
+	Address string `toml:"address"`
+	UID     int    `toml:"uid"`
+	GID     int    `toml:"gid"`
+}
+
+func (c *config) GetAddress() string {
+	return c.Address
+}
+
+func (c *config) SetAddress(s string) {
+	c.Address = s
+}
+
+func init() {
+	// Keep the default the same for compatibility
+	defaultAddress := defaults.DefaultAddress + ".ttrpc"
+	registry.Register(&plugin.Registration{
+		Type: plugins.ServerPlugin,
+		ID:   "ttrpc",
+		Requires: []plugin.Type{
+			plugins.TTRPCPlugin,
+			plugins.GRPCPlugin,
+		},
+		Config: &config{
+			Address: defaultAddress,
+		},
+		InitFn: func(ic *plugin.InitContext) (any, error) {
+			c := ic.Config.(*config)
+			if c.Address == "" {
+				return nil, plugin.ErrSkipPlugin
+			}
+
+			s, err := newTTRPCServer()
+			if err != nil {
+				return nil, err
+			}
+			// ttrpcService allows TTRPC services to be registered with the underlying server
+			type ttrpcService interface {
+				RegisterTTRPC(*ttrpc.Server) error
+			}
+			var hasService bool
+			ps, err := ic.GetByType(plugins.TTRPCPlugin) // ensure grpc plugin is initialized
+			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
+				return nil, err
+			}
+			for _, p := range ps {
+				if gs, ok := p.(ttrpcService); ok {
+					if err := gs.RegisterTTRPC(s); err != nil {
+						return nil, fmt.Errorf("failed to register ttrpc service: %w", err)
+					}
+					hasService = true
+				}
+			}
+			ps, err = ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
+			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
+				return nil, err
+			}
+			for _, p := range ps {
+				if gs, ok := p.(ttrpcService); ok {
+					if err := gs.RegisterTTRPC(s); err != nil {
+						return nil, fmt.Errorf("failed to register ttrpc service: %w", err)
+					}
+					hasService = true
+				}
+			}
+			if !hasService {
+				return nil, fmt.Errorf("no ttrpc services configured: %w", plugin.ErrSkipPlugin)
+			}
+			return server{
+				Server: s,
+				config: *c,
+			}, nil
+		},
+	})
+}
+
+type server struct {
+	*ttrpc.Server
+	config config
+}
+
+func (s server) Start(ctx context.Context) error {
+	// setup the ttrpc endpoint
+	tl, err := sys.GetLocalListener(s.config.Address, s.config.UID, s.config.GID)
+	if err != nil {
+		return fmt.Errorf("failed to get listener for main ttrpc endpoint: %w", err)
+	}
+
+	internal.Serve(ctx, tl, func(l net.Listener) error {
+		return s.Serve(context.WithoutCancel(ctx), l)
+	})
+
+	return nil
+}

cmd/containerd/builtins/builtins.go
@@ -30,6 +30,8 @@ import (
 	_ "github.com/containerd/containerd/v2/plugins/restart"
 	_ "github.com/containerd/containerd/v2/plugins/sandbox"
 	_ "github.com/containerd/containerd/v2/plugins/server/debug"
+	_ "github.com/containerd/containerd/v2/plugins/server/grpc"
+	_ "github.com/containerd/containerd/v2/plugins/server/ttrpc"
 	_ "github.com/containerd/containerd/v2/plugins/services/containers"
 	_ "github.com/containerd/containerd/v2/plugins/services/content"
 	_ "github.com/containerd/containerd/v2/plugins/services/diff"

cmd/containerd/command/config.go
@@ -125,18 +125,20 @@ func dumpConfig(cliContext *cli.Context) error {
 
 func platformAgnosticDefaultConfig() *srvconfig.Config {
 	return &srvconfig.Config{
-		Version: version.ConfigVersion,
-		Root:    defaults.DefaultRootDir,
-		State:   defaults.DefaultStateDir,
-		GRPC: srvconfig.GRPCConfig{
-			Address:        defaults.DefaultAddress,
-			MaxRecvMsgSize: defaults.DefaultMaxRecvMsgSize,
-			MaxSendMsgSize: defaults.DefaultMaxSendMsgSize,
-		},
+		Version:          version.ConfigVersion,
+		Root:             defaults.DefaultRootDir,
+		State:            defaults.DefaultStateDir,
 		DisabledPlugins:  []string{},
 		RequiredPlugins:  []string{},
 		StreamProcessors: streamProcessors(),
 		Imports:          []string{defaults.DefaultConfigIncludePattern},
+		Plugins: map[string]any{
+			"io.containerd.server.v1.grpc": map[string]any{
+				"address":               defaults.DefaultAddress,
+				"max_recv_message_size": defaults.DefaultMaxRecvMsgSize,
+				"max_send_message_size": defaults.DefaultMaxSendMsgSize,
+			},
+		},
 	}
 }
 

cmd/containerd/command/main.go
@@ -34,7 +34,6 @@ import (
 	_ "github.com/containerd/containerd/v2/core/metrics" // import containerd build info
 	"github.com/containerd/containerd/v2/core/mount"
 	"github.com/containerd/containerd/v2/defaults"
-	"github.com/containerd/containerd/v2/pkg/sys"
 	"github.com/containerd/containerd/v2/pkg/tracing"
 	"github.com/containerd/containerd/v2/version"
 	"github.com/containerd/errdefs"
@@ -165,16 +164,6 @@ can be used and modified as necessary as a custom configuration.`
 			return err
 		}
 
-		if config.GRPC.Address == "" {
-			return fmt.Errorf("grpc address cannot be empty: %w", errdefs.ErrInvalidArgument)
-		}
-		if config.TTRPC.Address == "" {
-			// If TTRPC was not explicitly configured, use defaults based on GRPC.
-			config.TTRPC.Address = config.GRPC.Address + ".ttrpc"
-			config.TTRPC.UID = config.GRPC.UID
-			config.TTRPC.GID = config.GRPC.GID
-		}
-
 		// Make sure top-level directories are created early.
 		if err := server.CreateTopLevelDirectories(config); err != nil {
 			return err
@@ -229,6 +218,7 @@ can be used and modified as necessary as a custom configuration.`
 		go func() {
 			defer close(chsrv)
 
+			// TODO: When to set grpc address from flag? Migration should be done first
 			server, err := server.New(ctx, config)
 			if err != nil {
 				select {
@@ -270,6 +260,8 @@ can be used and modified as necessary as a custom configuration.`
 		if err := server.Start(ctx); err != nil {
 			return err
 		}
+
+		// TODO: Move to server.Start(ctx) error
 		if config.Metrics.Address != "" {
 			l, err := net.Listen("tcp", config.Metrics.Address)
 			if err != nil {
@@ -277,26 +269,8 @@ can be used and modified as necessary as a custom configuration.`
 			}
 			serve(ctx, l, server.ServeMetrics)
 		}
-		// setup the ttrpc endpoint
-		tl, err := sys.GetLocalListener(config.TTRPC.Address, config.TTRPC.UID, config.TTRPC.GID)
-		if err != nil {
-			return fmt.Errorf("failed to get listener for main ttrpc endpoint: %w", err)
-		}
-		serve(ctx, tl, server.ServeTTRPC)
 
-		if config.GRPC.TCPAddress != "" {
-			l, err := net.Listen("tcp", config.GRPC.TCPAddress)
-			if err != nil {
-				return fmt.Errorf("failed to get listener for TCP grpc endpoint: %w", err)
-			}
-			serve(ctx, l, server.ServeTCP)
-		}
-		// setup the main grpc endpoint
-		l, err := sys.GetLocalListener(config.GRPC.Address, config.GRPC.UID, config.GRPC.GID)
-		if err != nil {
-			return fmt.Errorf("failed to get listener for main endpoint: %w", err)
-		}
-		serve(ctx, l, server.ServeGRPC)
+		// end s.Start
 
 		readyC := make(chan struct{})
 		go func() {
@@ -351,10 +325,6 @@ func applyFlags(cliContext *cli.Context, config *srvconfig.Config) error {
 			name: "state",
 			d:    &config.State,
 		},
-		{
-			name: "address",
-			d:    &config.GRPC.Address,
-		},
 	} {
 		if s := cliContext.String(v.name); s != "" {
 			*v.d = s
@@ -365,6 +335,35 @@ func applyFlags(cliContext *cli.Context, config *srvconfig.Config) error {
 				}
 				*v.d = absPath
 			}
+		}
+	}
+
+	if s := cliContext.String("address"); s != "" {
+		var (
+			grpcConfig  map[string]any
+			ttrpcConfig map[string]any
+		)
+		v, ok := config.Plugins["io.containerd.server.v1.grpc"]
+		if !ok {
+			grpcConfig = make(map[string]any)
+		} else if grpcConfig, ok = v.(map[string]any); !ok {
+			return fmt.Errorf("grpc plugin has invalid configuration: %w", errdefs.ErrInvalidArgument)
+		}
+		grpcConfig["address"] = s
+		config.Plugins["io.containerd.server.v1.grpc"] = grpcConfig
+
+		_, ok = config.Plugins["io.containerd.server.v1.ttrpc"]
+		if !ok {
+			ttrpcConfig = map[string]any{
+				"address": s + ".ttrpc",
+			}
+			if uid, ok := grpcConfig["uid"]; ok {
+				ttrpcConfig["uid"] = uid
+			}
+			if gid, ok := grpcConfig["gid"]; ok {
+				ttrpcConfig["gid"] = gid
+			}
+			config.Plugins["io.containerd.server.v1.ttrpc"] = ttrpcConfig
 		}
 	}
 

commit ef985f8628344d803d511adf12e5af7cf4606aef

server: tolerate failed gRPC plugins when starting listeners
The grpc, grpc-tcp, and ttrpc server plugins enumerated their services
through ic.GetByType, which short-circuits on the first plugin whose
Instance() returned an error. A single failed gRPC plugin (e.g. CRI
under rootless, which cannot watch /etc/cni/net.d) therefore prevented
the server plugins from initialising, leaving /run/containerd/containerd.sock
uncreated.

Iterate the plugin set directly and skip plugins that failed to
initialise, restoring the pre-c15ec2485 behaviour where the listener
is still created and only the failed services are missing.

Fixes: c15ec2485 ("Add server plugins for grpc and ttrpc")
Fixes: https://github.com/containerd/containerd/issues/13362

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>
Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

plugins/server/grpc/plugin.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -112,12 +111,19 @@ func init() {
 			}
 
 			s := grpc.NewServer(serverOpts...)
-			ps, err := ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
-			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
-				return nil, err
-			}
-			for _, p := range ps {
-				if gs, ok := p.(grpcService); ok {
+			// Iterate plugins directly rather than using GetByType, which
+			// short-circuits on the first plugin that failed to initialize.
+			// Plugins that failed (e.g. CRI under rootless) have already been
+			// logged and should not prevent the GRPC server from starting.
+			for _, p := range ic.Plugins().GetAll() {
+				if p.Registration.Type != plugins.GRPCPlugin {
+					continue
+				}
+				instance, err := p.Instance()
+				if err != nil {
+					continue
+				}
+				if gs, ok := instance.(grpcService); ok {
 					if err := gs.Register(s); err != nil {
 						return nil, fmt.Errorf("failed to register grpc service: %w", err)
 					}
@@ -218,13 +224,18 @@ func init() {
 			}
 
 			s := grpc.NewServer(serverOpts...)
-			ps, err := ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
-			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
-				return nil, err
-			}
+			// Iterate plugins directly rather than using GetByType, which
+			// short-circuits on the first plugin that failed to initialize.
 			var hasService bool
-			for _, p := range ps {
-				if gs, ok := p.(tcpService); ok {
+			for _, p := range ic.Plugins().GetAll() {
+				if p.Registration.Type != plugins.GRPCPlugin {
+					continue
+				}
+				instance, err := p.Instance()
+				if err != nil {
+					continue
+				}
+				if gs, ok := instance.(tcpService); ok {
 					if err := gs.RegisterTCP(s); err != nil {
 						return nil, fmt.Errorf("failed to register grpc service: %w", err)
 					}

plugins/server/ttrpc/plugin.go
@@ -18,7 +18,6 @@ package ttrpc
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -75,25 +74,20 @@ func init() {
 			type ttrpcService interface {
 				RegisterTTRPC(*ttrpc.Server) error
 			}
+			// Iterate plugins directly rather than using GetByType, which
+			// short-circuits on the first plugin that failed to initialize.
+			// Plugins that failed (e.g. CRI under rootless) have already been
+			// logged and should not prevent the TTRPC server from starting.
 			var hasService bool
-			ps, err := ic.GetByType(plugins.TTRPCPlugin) // ensure grpc plugin is initialized
-			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
-				return nil, err
-			}
-			for _, p := range ps {
-				if gs, ok := p.(ttrpcService); ok {
-					if err := gs.RegisterTTRPC(s); err != nil {
-						return nil, fmt.Errorf("failed to register ttrpc service: %w", err)
-					}
-					hasService = true
+			for _, p := range ic.Plugins().GetAll() {
+				if p.Registration.Type != plugins.TTRPCPlugin && p.Registration.Type != plugins.GRPCPlugin {
+					continue
 				}
-			}
-			ps, err = ic.GetByType(plugins.GRPCPlugin) // ensure grpc plugin is initialized
-			if err != nil && !errors.Is(err, plugin.ErrPluginNotFound) {
-				return nil, err
-			}
-			for _, p := range ps {
-				if gs, ok := p.(ttrpcService); ok {
+				instance, err := p.Instance()
+				if err != nil {
+					continue
+				}
+				if gs, ok := instance.(ttrpcService); ok {
 					if err := gs.RegisterTTRPC(s); err != nil {
 						return nil, fmt.Errorf("failed to register ttrpc service: %w", err)
 					}